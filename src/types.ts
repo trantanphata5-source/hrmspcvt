34eaf61b9da98a0d4b90d4d8deca8f50db291b59
trantanphata5-source <trantanphata5@gmail.com>
2026-05-22 00:30:47 +0700

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export interface Employee {
+  stt: string;
+  msnv: string;
+  avatarUrl: string;
+  fullName: string;
+  birthDate: string;
+  phone: string;
+  email: string;
+  gender: string;
+  deptCode: string;
+  deptShort: string;
+  department: string;
+  team: string;
+  roleCode: string;
+  role: string;
+  educationLevel: string;
+  educationMajor: string;
+  partyBranch: string;
+  partyMember: string;
+  unionMember: string;
+  youthUnionMember: string;
+  base: string;
+  oldBase: string;
+  address: string;
+}
